39acedec69915d459adcf226e8c0051a382ad316
Artem <artem@gmail.com>
2021-01-10 18:24:01 +0200

    Added validator, refactored code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query, Path
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, Response
 from email_validator import validate_email, EmailNotValidError
+import json
 
 app = FastAPI()
 
@@ -12,5 +13,12 @@ async def root():
 
 @app.get("/build_graph/user_id={user_id}")
 async def build_graph(user_id: int = Path(None, title="The ID of the item to get", gt=0), email: str = Query(None, max_length=50)):
-
-    return {"item_id": user_id, 'email':email}
+    try:
+        if email:
+            validate_email(email)
+    except EmailNotValidError as e:
+        return Response(str(e),status_code=400)
+    except Exception as e:
+        return Response(str(e), status_code=500)
+    data = {'user_id': user_id, 'email':email}
+    return Response(json.dumps(data, default=lambda x: x.__dict__))
