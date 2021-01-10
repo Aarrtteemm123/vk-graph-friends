069f201fd8dfec1e78458e7a3b5d3d186ab7ca8e
Artem <artem@gmail.com>
2021-01-10 18:11:17 +0200

    Added path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 from starlette.responses import HTMLResponse
+from email_validator import validate_email, EmailNotValidError
 
 app = FastAPI()
 
@@ -9,7 +10,7 @@ async def root():
         html = file.read()
     return HTMLResponse(content=html, status_code=200)
 
-@app.get("/build_graph/user_id={user_id}&email={email}")
-async def build_graph(user_id,email):
-    print(user_id,email)
+@app.get("/build_graph/user_id={user_id}")
+async def build_graph(user_id: int = Path(None, title="The ID of the item to get", gt=0), email: str = Query(None, max_length=50)):
+
     return {"item_id": user_id, 'email':email}
