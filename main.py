e96179a22a3a9bb92b432748127e4c06f392b549
Artem <artem@gmail.com>
2021-01-11 17:27:03 +0200

    Added background taskk

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI, Query, Path
-from starlette.responses import HTMLResponse, Response
+import io
+import time
+
+from fastapi import FastAPI, Query, Path, BackgroundTasks
+from starlette.responses import HTMLResponse, Response, StreamingResponse
 from email_validator import validate_email, EmailNotValidError
-import json
 
 app = FastAPI()
+
+def build_graph(user_id, email: str=None):
+    if email is not None:
+        pass
+    else:
+        pass
+    return 'ok'
 
 @app.get("/")
 async def root():
@@ -11,14 +20,19 @@ async def root():
         html = file.read()
     return HTMLResponse(content=html, status_code=200)
 
-@app.get("/build_graph/user_id={user_id}")
-async def build_graph(user_id: int = Path(None, title="The ID of the item to get", gt=0), email: str = Query(None, max_length=50)):
+@app.get("/graph/user_id={user_id}")
+async def graph(background_tasks: BackgroundTasks,
+                      user_id: int = Path(None, title="ID of the Vk user", gt=0),
+                      email: str = Query(None, max_length=50)):
     try:
         if email:
             validate_email(email)
+            background_tasks.add_task(build_graph, user_id, email)
+            return Response({'Answer':'Your request accepted, we will send result to your email'})
+        else:
+            result_img = build_graph(user_id)
+            return StreamingResponse(io.BytesIO(result_img.tobytes()), media_type="image/png")
     except EmailNotValidError as e:
         return Response(str(e),status_code=400)
     except Exception as e:
         return Response(str(e), status_code=500)
-    data = {'user_id': user_id, 'email':email}
-    return Response(json.dumps(data, default=lambda x: x.__dict__))
